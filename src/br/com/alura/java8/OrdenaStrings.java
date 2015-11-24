6ee29c83d6e548e32751a0785a66b642197fd06a
JhonatanRocha <jcristianrocha@gmail.com>
2015-11-24 13:58:38 -0200

    Lesson 2 - Deleting unused classes and implementing same methods with Lambda expression.

--- src/br/com/alura/java8/OrdenaStrings.java
+++ src/br/com/alura/java8/OrdenaStrings.java
@@ -9,35 +9,48 @@ import java.util.function.Consumer;
 
 public class OrdenaStrings {
 	public static void main(String[] args) {
-		
+
 		List<String> palavras = new ArrayList<String>();
-		
-		Comparator<String> comparador = new ComparadorPorTamanho();
+
 		palavras.add("video aula");
 		palavras.add("pc");
 		palavras.add("java");
+
+		// Utilizando a interface do Java 8 para ordenar.
+		// Implementação através do Lambida
+		/*palavras.sort((s1, s2) -> {
+			if(s1.length() < s2.length())
+				return -1;
+			if(s1.length() > s2.length())
+				return 1;
+			else
+				return 0;
+		});*/
 		
-		//Utilizando a interface do Java 8 para ordenar.
-		palavras.sort(comparador);
 		
+		//A mesma implementação acima porém mais enchuta
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+
 		/*
 		 * Ordenação Antiga de String da forma antiga.
 		 * 
-		Collections.sort(palavras, comparador);
-		*/
-		
+		 * Collections.sort(palavras, comparador);
+		 */
+
 		System.out.println(palavras);
-	
+
 		/*
 		 * Forma Antiga de Iterar uma coleção de Strings.
 		 * 
-		 * for (String palavra : palavras) {
-			System.out.println(palavra);
-		   }
-		*/
+		 * for (String palavra : palavras) { System.out.println(palavra); }
+		 */
+
+		// Utilizando a interface do Java 8 para iterarção.
+		// Implementação através do Lambida
+		palavras.forEach(palavra -> System.out.println(palavra));
 		
-		//Utilizando a interface do Java 8 para iterarção.
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+		// Implementação através do Lambida de uma Thread
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
 	}
 }
