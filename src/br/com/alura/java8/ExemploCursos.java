f2562b135afa7b8ed05eccf42eb28a6f7da4a0c8
JhonatanRocha <jcristianrocha@gmail.com>
2015-11-30 16:00:23 -0200

    Lesson 5 - Going deep with the Stream API and Collectors.

--- src/br/com/alura/java8/ExemploCursos.java
+++ src/br/com/alura/java8/ExemploCursos.java
@@ -3,6 +3,10 @@ package br.com.alura.java8;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ExemploCursos {
@@ -10,7 +14,7 @@ public class ExemploCursos {
 		List<Curso> cursos = new ArrayList<Curso>();
 		cursos.add(new Curso("Python", 45));
 		cursos.add(new Curso("JavaScript", 150));
-		cursos.add(new Curso("Java 8", 114));
+		cursos.add(new Curso("Java 8", 113));
 		cursos.add(new Curso("C++", 55));
 		
 		//Ordenando por número de alunos utilizando Lambda
@@ -30,14 +34,35 @@ public class ExemploCursos {
 		//cursos.stream().filter(c -> c.getAlunos() >= 100).map(c -> c.getAlunos()).forEach(total -> System.out.println(total));
 
 		//Fazendo a mesma função acima, porém utilizando method reference
-		cursos.stream().filter(c -> c.getAlunos() >= 100).map(Curso::getAlunos).forEach(System.out::println);
+		//cursos.stream().filter(c -> c.getAlunos() >= 100).map(Curso::getAlunos).forEach(System.out::println);
 		
-		int soma = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
-		System.out.println("Total de alunos por cursos com mais de 100 alunos: " + soma);
+		//int soma = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
+		//System.out.println("Total de alunos por cursos com mais de 100 alunos: " + soma);
 	
-		//Stream de String que contém os nomes de todos os cursos.
-		Stream<String> nomes = cursos.stream().map(Curso::getNome);
+		//OptionalDouble media = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).average();
 		
-		cursos.stream().filter(c -> c.getAlunos() > 50).map(Curso::getAlunos).forEach(System.out::println);
+		//Stream de String que contém os nomes de todos os cursos.
+		//Stream<String> nomes = cursos.stream().map(Curso::getNome);
+		
+		//cursos.stream().filter(c -> c.getAlunos() > 50).map(Curso::getAlunos).forEach(System.out::println);
+	
+		
+		//Optional<Curso> optionalCurso = cursos.stream().filter(c -> c.getAlunos() >= 100).findAny();
+		//Curso curso = optionalCurso.orElse(null);
+		//cursos.stream().filter(c -> c.getAlunos() >= 100).findAny().ifPresent(c -> System.out.println(c.getNome()));
+	
+		//Atribuindo o resultado filtrado para uma lista.
+		//cursos = cursos.stream().filter(c -> c.getAlunos() >= 100).collect(Collectors.toList());
+		
+		//cursos.forEach(c -> System.out.println(c.getNome()));
+
+		OptionalDouble media = cursos.stream().mapToInt(Curso::getAlunos).average();
+		System.out.println("Média de alunos: " + media.getAsDouble());		
+		
+		Map<String, Integer> mapaCursos = cursos.stream().filter(c -> c.getAlunos() >= 100).collect(Collectors.toMap(c -> c.getNome(),c -> c.getAlunos()));
+		System.out.println(mapaCursos);
+		
+		cursos.stream().filter(c -> c.getAlunos() >= 100).collect(Collectors.toMap(c -> c.getNome(),c -> c.getAlunos())).forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos."));
+
 	}
 }
